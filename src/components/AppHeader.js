1a51343512a648464ae863b578ebac2a32dc6586
Tracie Feng <wf17@students.waikato.ac.nz>
2020-06-04 09:48:30 +0000

    COMPX341-20 A4 VERSION 1-Final

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by city name in New Zealand</p>
+        <p className="p-3 text-light h3 bg-primary">Current weather by City Name(New Zealand)</p>
     </div>
 }
   
