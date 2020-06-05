624ad43c460146011144d50b5930cf4233a21353
Tracie Feng <wf17@students.waikato.ac.nz>
2020-06-05 05:04:33 +0000

    Edit code

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch'
 import CityResponse from '../components/CityResponse';
 import City from '../components/City';
-import Map from '../components/Map';
 
 function AppContainer(props) {
 
@@ -32,11 +31,6 @@ function AppContainer(props) {
                 <CityResponse responseData={responseData} clearResponse={clearResponse}/>
                 <div className="col-sm-2"></div>
             </div>
-            <div className="row mt-4">
-                <div className="col-sm-2"></div>
-                <Map responseData={responseData} clearResponse={clearResponse}/>
-                <div className="col-sm-2"></div>
-            </div> 
         </div>
     );
 }
