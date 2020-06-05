8aec8ba7a947da974d3791a65f3a035a10e6dbf9
Tracie Feng <wf17@students.waikato.ac.nz>
2020-06-05 02:28:11 +0000

    revert changes

---
+++ src/components/Map.js
@@ -0,0 +1,25 @@
+//import React, { useState } from 'react';
+//
+//function Map(props) {
+//
+//mapboxgl.accessToken = 'pk.eyJ1Ijoid2YxNyIsImEiOiJja2Iwc2VncW8wYjU1MnNtdDYxeGZtbjdoIn0.j4N-CO6JKlROIO2y9eR4eg';
+//    var map = new mapboxgl.Map({
+//        container: 'map',
+//        style: 'mapbox://styles/mapbox/streets-v11',
+//        center: [175.28, -37.78],
+//        zoom: 8
+//    });
+//
+//    var marker = new mapboxgl.Marker()
+//        .setLngLat([175.28, -37.78])
+//        .addTo(map);
+//
+//
+//	if(props.responseData.cod === 200) {
+//        return (
+//        <div id="map"></div>
+//        )
+//    }
+//}
+//
+//export default Map
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch'
 import CityResponse from '../components/CityResponse';
 import City from '../components/City';
-//import Map from '../components/Map';
 
 function AppContainer(props) {
 
